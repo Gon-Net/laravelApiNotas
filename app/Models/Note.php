9a3b64aebdf684a2191ecbacf0327f7e815f348c
jcori <gonzalocori@gmail.com>
2024-12-03 23:45:26 -0400

    Create routes

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Note extends Model
 {
-    //
+    use softDeletes;
+
+    protected $fillable = ['title','content','date','user_id'];
+    protected $hidden = ['created_at','updated_at','deleted_at'];
+    
+    function User(){
+        return $this->belongsTo(User::class);
+    }    
+
 }
